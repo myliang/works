21f64a244d6bdbe662dfd3924af42ff664098924
yuliang.liang <myliang@localhost.localdomain>
2014-08-25 18:42:52 +0800

    icons

---
+++ xoy_wxp/main_frame.py
@@ -0,0 +1,20 @@
+#!/bin/python
+# coding = utf-8
+
+import wx
+
+class MainFrame(wx.Frame):
+  def __init__(self, parent, title):
+    wx.Frame.__init__(self, parent, title = title, size = (500, 600))
+    toolBar = self.CreateToolBar()
+    # new, start, stop, delete, setting
+    qtool = toolBar.AddLabelTool(wx.ID_EXIT, '', wx.Bitmap("/home/myliang/workspaces/wxpython/test/exit.png"))
+    toolBar.Realize()
+
+    # self.SetMenuBar(menuBar)
+    self.Show(True)
+
+if __name__ == '__main__':
+  app = wx.App(False)
+  frame = MainFrame(None, "xoy")
+  app.MainLoop()
