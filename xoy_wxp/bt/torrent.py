bf91d27ee6a246183d52bbb32fcab1e7211a852d
yuliang.liang <myliang@localhost.localdomain>
2014-08-29 17:53:25 +0800

    bt

--- xoy_wxp/bt/torrent.py
+++ xoy_wxp/bt/torrent.py
@@ -1,6 +1,50 @@
 #!/bin/python
 # coding = utf-8
 
+import sys
+import hashlib
+
+def toTorrentFromFile(filename):
+  return Torrent(toBEncodeFromFile(filename))
+
+# convert data to becode from file
+def toBEncodeFromFile(filename):
+  f = None
+  try:
+    f = open(filename, 'rb')
+    read_bytes = f.read()
+    return BEncode(read_bytes)
+  finally:
+    if f:
+      f.close()
+  return None
+
+# class torrent
+class Torrent:
+  def __init__(self, bencode):
+    d = bencode.parse()
+    self.comment = d['comment'].decode('utf-8')
+    self.creation_date = d['creation date']
+    self.created_by = d['created by'].decode('utf-8')
+    self.encoding = d['encoding'].decode('utf-8')
+
+    self.announces = []
+    if d['announce-list']:
+      for v in d['announce-list']:
+        self.announces.append(v[0])
+    else:
+      self.announces.append(d['announce'].decode('utf-8'))
+
+    # info hash
+    self.info_hash = hashlib.sha1(bencode.info).hexdigest()
+    self.name = d['info']['name']
+    self.pieces = d['info']['pieces']
+    self.piece_length = d['info']['piece length']
+    self.files = d['info']['files']
+    print self.files
+
+
+# class bencode
 class BEncode:
 
   def __init__(self, value):
@@ -10,21 +54,25 @@ class BEncode:
   def parse(self):
     v = self.indexValue()
     if v == 'd':
-      return parseDict()
+      return self.parseDict()
     elif v == 'l':
-      return parseList()
+      return self.parseList()
     elif v == 'i':
-      return parseInt()
+      return self.parseInt()
     else:
-      return parseString()
+      return self.parseString()
 
 
   def parseDict(self):
     kv = {}
     self.indexInc()
     while 'e' != self.indexValue():
-      key = parseString()
-      value = parse()
+      key = self.parseString()
+      begin = self.index
+      value = self.parse()
+      if 'info' == key:
+        self.info = self.value[begin:self.index]
+
       kv[key] = value
     self.indexInc()
     return kv
@@ -32,21 +80,23 @@ class BEncode:
   def parseList(self):
     v = []
     self.indexInc()
-    while 'e' != sef.indexValue():
-      v.append(parse())
+    while 'e' != self.indexValue():
+      v.append(self.parse())
     self.indexInc()
     return v
 
   def parseString(self):
-    length = readInt()
+    length = self.readInt()
     self.indexInc()
     begin = self.index
     self.index += length
-    return self.value[begin, self.index]
+    return self.value[begin:self.index]
 
   def parseInt(self):
     self.indexInc()
-    return readInt()
+    v = self.readInt()
+    self.indexInc()
+    return v
 
   def indexValue(self):
     return self.value[self.index]
@@ -57,6 +107,10 @@ class BEncode:
   def readInt(self):
     v = 0
     while self.indexValue().isdigit():
-      v = v * 10 + (self.indexValue() - '0')
-      self.index++
+      v = v * 10 + int(self.indexValue())
+      self.indexInc()
     return v
+
+
+if __name__ == '__main__':
+  toTorrentFromFile(sys.argv[1])
