1506961b7737c6947fbd8ab5239da1151b7a216b
yuliang.liang <myliang@localhost.localdomain>
2014-09-22 19:42:39 +0800

    add config

---
+++ xoy_lc/src/config.h
@@ -0,0 +1,6 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define LISTEN_PORT 6881
+
+#endif /* end of include guard: _CONFIG_H_ */
--- xoy_lc/src/tracker.c
+++ xoy_lc/src/tracker.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "config.h"
 #include "bencode.h"
 #include "torrent.h"
 #include "tracker.h"
 #include "io_http.h"
+
+#define TRACKER_NUMWANT 200
+
+static char *tracker_events[] = {
+  "started",
+  "stopped",
+  "completed"
+};
 
 static void request_trackers_with_http(const char* url, b_torrent* tptr, b_peer* pptr, int timeout);
 static void request_trackers_with_udp(const char* url, b_torrent* tptr, b_peer* pptr, int timeout);
@@ -26,9 +35,9 @@ void request_trackers(b_torrent* tptr, b_peer* pptr, int timeout) {
 static void request_trackers_with_http(const char* url, b_torrent* tptr, b_peer* pptr, int timeout) {
   char full_url[1024];
   snprintf(full_url, sizeof(full_url) - 1,
-      "%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&event=%s&numwant=%d",
+      "%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&event=%s&numwant=%d&compact=1",
       url, http_uri_hex(tptr->info_hash, 20), http_uri_hex(tptr->peer_id, 20),
-      6881, 0, 0, 10240000, "started", 200);
+      LISTEN_PORT, 0, 0, 10240000, tracker_events[0], TRACKER_NUMWANT);
   full_url[sizeof(full_url) - 1] = '\0';
   printf("url=%s\n", full_url);
 
